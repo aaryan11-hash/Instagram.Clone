234d6b6cf36ca02f1c2df5fe481ea45ae32e10a3
aaryan11-hash <aaryan2000dpspune@gmail.com>
2020-10-27 23:51:34 +0530

    finished password and email change endpoints and db layer #ends 14

--- src/main/java/com/aaryan/Instagram/Clone/Controllers/UserAccSettingController.java
+++ src/main/java/com/aaryan/Instagram/Clone/Controllers/UserAccSettingController.java
@@ -11,7 +11,7 @@ import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//ignore commit
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/account/setting")
